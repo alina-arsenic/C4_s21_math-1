cec452fcee64d79bd6c4bf839a41c489f113b9af
weaveryu <weaveryu@student.21-school.ru>
2022-08-05 01:03:23 +0300

    s21_math.h 1.1

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,19 +1,19 @@
 #ifndef SRC_S21_MATH_H_
 #define SRC_S21_MATH_H_
 
-# define S21_M_E        2.71828182845904523536   // e
-# define S21_M_LOG2E    1.44269504088896340736   // log_2 e
-# define S21_M_LOG10E   0.434294481903251827651  // log_10 e
-# define S21_M_LN2      0.693147180559945309417  // log_e 2
-# define S21_M_LN10     2.30258509299404568402   // log_e 10
-# define S21_M_PI       3.14159265358979323846   // pi
-# define S21_M_PI_2     1.57079632679489661923   // pi/2
-# define S21_M_PI_4     0.785398163397448309616  // pi/4
-# define S21_M_1_PI     0.318309886183790671538  // 1/pi
-# define S21_M_2_PI     0.636619772367581343076  // 2/pi
-# define S21_M_2_SQRTPI 1.12837916709551257390   // 2/sqrt(pi)
-# define S21_M_SQRT2    1.41421356237309504880   // sqrt(2)
-# define S21_M_SQRT1_2  0.707106781186547524401  // 1/sqrt(2)
+#define S21_M_E        2.71828182845904523536   // e
+#define S21_M_LOG2E    1.44269504088896340736   // log_2 e
+#define S21_M_LOG10E   0.434294481903251827651  // log_10 e
+#define S21_M_LN2      0.693147180559945309417  // log_e 2
+#define S21_M_LN10     2.30258509299404568402   // log_e 10
+#define S21_M_PI       3.14159265358979323846   // pi
+#define S21_M_PI_2     1.57079632679489661923   // pi/2
+#define S21_M_PI_4     0.785398163397448309616  // pi/4
+#define S21_M_1_PI     0.318309886183790671538  // 1/pi
+#define S21_M_2_PI     0.636619772367581343076  // 2/pi
+#define S21_M_2_SQRTPI 1.12837916709551257390   // 2/sqrt(pi)
+#define S21_M_SQRT2    1.41421356237309504880   // sqrt(2)
+#define S21_M_SQRT1_2  0.707106781186547524401  // 1/sqrt(2)
 
 int s21_abs(int x);  // вычисляет абсолютное значение целого числа
 long double s21_acos(double x);   // вычисляет арккосинус
