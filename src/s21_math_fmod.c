c7394234edce06205c4a2120dba9ddb47339627d
Worker Watcher <workerwa@at-j6.msk.21-school.ru>
2022-08-09 15:28:49 +0300

    wertghfdew

--- src/s21_math_fmod.c
+++ src/s21_math_fmod.c
@@ -4,15 +4,14 @@
 long double s21_fmod(double x, double y);
 
 int main () {
-    double x = 53.5;
-    double y = 7.61;
+    double x = 5323.5323;
+    double y = 71.61323;
     printf("%Lf\n", s21_fmod(x, y));
     printf("%f\n", fmod(x, y));
     return 0;
 }
 
 long double s21_fmod(double x, double y) {
-	int i_x = (int) x;
-	int i_y = (int) y;
-	return (long double)((i_x % i_y) + ((x - i_x) - (y - i_y)));
+    int div = x / y;
+    return x - div * y;
 }
