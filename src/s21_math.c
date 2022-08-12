934106ac6043dd65fe73864b6bc4520b947426ec
Your Name <teresecl@student.21-school.ru>
2022-08-12 17:34:00 +0300

    no tests leaks on mac

--- src/s21_math.c
+++ src/s21_math.c
@@ -2,44 +2,54 @@
 #include <math.h>
 
 long double s21_acos(double x) {
-	long double result = 0;
-	if (x > 1.0 || x < -1.0)
-		result = S21_NAN;
-	else if (x == 1)
-		result = 0.0;
-	else if (x == -1)
-		result = S21_PI;
-	else
-		result = S21_PI / 2 - asin(x);
-	return result;
+    long double result = 0.0;
+    if (x == 1.0) {
+        return 0;
+    } else if (x == -1.0) {
+        return S21_PI;
+    } else if (x == 0) {
+        return S21_CONST_1;
+    } else if (x == S21_CONST_SQ) {
+        return S21_CONST_2;
+    } else if (x == -S21_CONST_SQ) {
+        return 3 * S21_CONST_2;
+    }
+
+    if (0.0 < x && x < 1.0) {
+        result = atan(sqrt(1 - x * x) / x);
+    } else if (-1. < x && x < 0.) {
+        result = S21_PI + atan(sqrt(1 - x * x) / x);
+    } else {
+        result = S21_NAN;
+    }
+
+    return result;
 }
 
 long double s21_exp(double x) {
-    long double res = 0;
-    if (x < 0) {
-        res = 1 / s21_exp((-1) * x);
-    } else {
-        for (int k = 0; k < 2000; ++k) {
-            double current = 1;
-            for (int j = 1; j <= k; ++j) {
-                current *= x;
-                current /= j;
-            }
-            res += current;
-        }
-    }
-    return res;
+  if (S21_isnan(x)) return S21_NAN;
+  if (S21_isinf(x)) return x < 0 ? 0 : S21_INF;
+  int indic = x < 0;
+  if (indic) x *= -1;
+  long double result = 1;
+  double tmp = x;
+  unsigned flag = 1;
+  while (fabs(tmp) >= S21_EPS) {
+    result += tmp;
+    tmp = (tmp * x) / ++flag;
+  }
+  return indic ? (1 / result) : result;
 }
 
 long double s21_sin(double x) {
     x = fmod(x, 2 * S21_PI);
     long double result = 0;
-    long double temp = x;
+    long double tmp = x;
     unsigned flag = 1;
     while (flag < 1000) {
-        result += temp;
+        result += tmp;
         flag += 2;
-        temp *= -(x * x) / ((flag - 1) * flag);
+        tmp *= -(x * x) / ((flag - 1) * flag);
     }
     return result;
 }
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,12 +1,21 @@
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <float.h>
-#include <limits.h>
 
 #define S21_EPS 1e-6
 #define S21_PI 3.14159265358979323846264338327950288
+#define S21_CONST_1 1.570796326794896558
+#define S21_CONST_2 0.785398163397448309
+#define S21_CONST_SQ 0.7071067811865475244
 #define S21_NAN 0.0 / 0.0
+#define S21_INF 1.0 / 0.0
+#define S21_isnan(x) (!(x >= 0) && !(x < 0))
+#define S21_isinf(x) ((x == S21_INF) || (x == -S21_INF))
 
 long double s21_acos(double x);
 long double s21_exp(double x);
 long double s21_sin(double x);
+
+#endif  // SRC_S21_MATH_H_
